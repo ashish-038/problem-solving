b35bc2c5416658984492421bdb6659d84717454c
Kangsoo Lee <kangsoo.s.lee@gmail.com>
2018-04-10 15:17:20 +0900

    code: Create BinaryGap.swift

---
+++ lesson1-iterations/BinaryGap.swift
@@ -0,0 +1,32 @@
+import Foundation
+import Glibc
+
+public func solution(_ N: Int) -> Int {
+    let binaryArray = makeBinaryArray(N)
+    var gaps = [Int]()
+    var gap = 0
+    
+    for (index, binary) in binaryArray.enumerated() {
+        if binary == 0 {
+            guard index != binaryArray.endIndex else { break }
+            gap += 1
+        } else {
+            gaps.append(gap)
+            gap = 0
+        }
+    }
+    
+    return gaps.max()!
+}
+
+private func makeBinaryArray(_ N: Int) -> [Int] {
+    var integer = N
+    var array = [Int]()
+    
+    while integer != 0 {
+        array.insert(integer % 2, at: 0)
+        integer /= 2
+    }
+    
+    return array
+}
