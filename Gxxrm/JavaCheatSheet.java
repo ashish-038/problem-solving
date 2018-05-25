3f275be198f4619bf7d125598adaf0d5cb7da4b6
oaksong <kangsoo.s.lee@gmail.com>
2018-05-26 06:55:18 +0900

    code: Create a cheat sheet for Java

---
+++ Gxxrm/JavaCheatSheet.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+Scanner scanner = new Scanner(System.in);
+scanner.nextInt();
+scanner.nextLine();
+
+System.out.println();
+System.out.println(Arrays.toString(array));
+System.out.println(Arrays.asList(map));
+
+StringBuilder builder = new StringBuilder();
