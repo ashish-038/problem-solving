b78490155348d9efccc6060c25d1d589021b5ef3
oaksong <kangsoo.s.lee@gmail.com>
2018-05-26 18:09:31 +0900

    code: Update JavaCheatSheet

--- JavaCheatSheet.java
+++ JavaCheatSheet.java
@@ -1,11 +1,15 @@
 import java.util.*;
+import java.io.*;
+import java.lang.*;
 
 Scanner scanner = new Scanner(System.in);
 scanner.nextInt();
 scanner.nextLine();
 
 System.out.println();
-System.out.println(Arrays.toString(array));
-System.out.println(Arrays.asList(map));
+System.out.println(Arrays.toString(array)); // print array
+System.out.println(Arrays.asList(map)); // print hashmap
 
 StringBuilder builder = new StringBuilder();
+builder.setLength(0); // clear builder
+StringJoiner joiner = new StringJoiner(" ");
