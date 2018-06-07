27abb9758929e2b6f30bf71fdf4f1ad9c2fcb8db
oaksong <kangsoo.s.lee@gmail.com>
2018-06-08 05:09:47 +0900

    chore: Edit wrong difficulty level in comment

--- Codility/MaxCounters.java
+++ Codility/MaxCounters.java
@@ -1,7 +1,7 @@
 /*
     MaxCounters
     - Calculate the values of counters after applying all alternating operations: increase counter by 1; set value of all counters to current maximum.
-    - Difficulty Level: PAINLESS
+    - Difficulty Level: RESPECTABLE
 
     Results
     - Task Score: 100%
